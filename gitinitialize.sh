79c7f764e9f35c270f0260b3f00d86f9a24a8a95
Owen Medina <owen@owens-mbp.home>
2020-09-07 18:29:04 -0700

    First commit

---
+++ gitinitialize.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+
+# This file should be sourced
+echo -n "Enter path to local repository > "
+read path
+cd "$path"
+
+# echo -n "Enter repository URL > "
+# read url
+# git init
+# git add .
+# git commit -m "First commit"
+# git remote add origin "$url"
+# git remote -v
+# git push -u origin master
