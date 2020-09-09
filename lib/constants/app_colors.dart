e4a4ae491bcf3f4e0cc5e0fe2a79b13e4383b53f
Owen Medina <owen@owens-mbp.home>
2020-09-09 14:41:35 -0700

    Re-add app name changes and missing dependencies

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const darkBlue = Color.fromRGBO(71, 126, 194, 1);
+  static const mediumBlue = Color.fromRGBO(113, 162, 214, 1);
+  static const lightBlue = Color.fromRGBO(135, 180, 225, 1);
+
+  static const Map<int, Color> darkBlueColorCodes = {
+    50: Color.fromRGBO(71, 126, 194, .1),
+    100: Color.fromRGBO(71, 126, 194, .2),
+    200: Color.fromRGBO(71, 126, 194, .3),
+    300: Color.fromRGBO(71, 126, 194, .4),
+    400: Color.fromRGBO(71, 126, 194, .5),
+    500: Color.fromRGBO(71, 126, 194, .6),
+    600: Color.fromRGBO(71, 126, 194, .7),
+    700: Color.fromRGBO(71, 126, 194, .8),
+    800: Color.fromRGBO(71, 126, 194, .9),
+    900: Color.fromRGBO(71, 126, 194, 1),
+  };
+
+  static const darkBlueHexCode = 0xFF477EC2;
+  static const MaterialColor materialDarkBlue = MaterialColor(darkBlueHexCode, darkBlueColorCodes);
+
+  static const Map<int, Color> mediumBlueColorCodes = {
+    50: Color.fromRGBO(113, 162, 214, .1),
+    100: Color.fromRGBO(113, 162, 214, .2),
+    200: Color.fromRGBO(113, 162, 214, .3),
+    300: Color.fromRGBO(113, 162, 214, .4),
+    400: Color.fromRGBO(113, 162, 214, .5),
+    500: Color.fromRGBO(113, 162, 214, .6),
+    600: Color.fromRGBO(113, 162, 214, .7),
+    700: Color.fromRGBO(113, 162, 214, .8),
+    800: Color.fromRGBO(113, 162, 214, .9),
+    900: Color.fromRGBO(113, 162, 214, 1),
+  };
+
+  static const mediumBlueHexCode = 0xFF71A2D6;
+  static const MaterialColor materialMediumBlue = MaterialColor(mediumBlueHexCode, mediumBlueColorCodes);
+
+  static const Map<int, Color> lightBlueColorCodes = {
+    50: Color.fromRGBO(135, 180, 225, .1),
+    100: Color.fromRGBO(135, 180, 225, .2),
+    200: Color.fromRGBO(135, 180, 225, .3),
+    300: Color.fromRGBO(135, 180, 225, .4),
+    400: Color.fromRGBO(135, 180, 225, .5),
+    500: Color.fromRGBO(135, 180, 225, .6),
+    600: Color.fromRGBO(135, 180, 225, .7),
+    700: Color.fromRGBO(135, 180, 225, .8),
+    800: Color.fromRGBO(135, 180, 225, .9),
+    900: Color.fromRGBO(135, 180, 225, 1),
+  };
+
+  static const lightBlueHexCode = 0xFF87B4E1;
+  static const MaterialColor materialLightBlue = MaterialColor(lightBlueHexCode, lightBlueColorCodes);
+}
---
+++ lib/constants/strings_constants.dart
@@ -0,0 +1,24 @@
+class StringConstants {
+  // Error-handling
+  static const success = 'Success';
+  static const undefined = 'Undefined error';
+  static const invalidEmail = 'You have entered an invalid email.';
+  static const wrongPassword = 'You have entered the wrong password';
+  static const userNotFound = 'User with these credentials was not found';
+  static const userDisabled = 'User with these credentials is currently disabled';
+  static const tooManyRequests = 'Too many requests being sent. Please try again later.';
+  static const operationNotAllowed = 'Operation is not allowed';
+
+  // Login Screen
+  static const logIn = 'Log In';
+  static const signUp = 'Sign Up';
+  static const emailAlreadyExists = 'User with this email already exists';
+  static const logInPrompt = 'Already have an account? Login instead.';
+  static const signUpPrompt = 'Sign Up';
+
+  // Home Screen
+  static const homeScreenTitle = 'Home';
+
+  // Meeting Screen
+  static const meetingScreenTitle = 'Join a Meeting';
+}
