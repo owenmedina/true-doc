18dec3ec8f085c47889c58f4922707b677845999
Owen Medina <owen@owens-mbp.home>
2020-09-15 10:04:20 -0700

    Fix Calls methods' return types

--- lib/services/calls.dart
+++ lib/services/calls.dart
@@ -3,25 +3,25 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class Calls {
   Future<void> createCall(String physicianId, String patientId) async {
     try {
-      final response = await FirebaseFirestore.instance.collection('calls').add({
+      await FirebaseFirestore.instance.collection('calls').add({
         'physician': physicianId,
         'patient': patientId,
         'start': DateTime.now().toIso8601String(),
       });
-      return response;
     } catch (e) {
       print('Caught error in Calls.createCall(): $e');
-      return null;
+      throw e;
     }
   }
 
-  Future<void> endCall(String callId) {
+  void endCall(String callId) {
     try {
       FirebaseFirestore.instance.collection('calls').doc(callId).set({
         'end': DateTime.now().toIso8601String(),
       });
     } catch (e) {
       print('Caught error in Calls.endCall(): $e');
+      throw e;
     }
   }
 }
